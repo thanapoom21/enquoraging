4c788d43128f17d701debede98423839b10dbc83
thanapoom21 <thanapoom.p.music@gmail.com>
2023-03-16 00:59:12 -0700

    add quote data and APIs requesting for quotes

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+export default {
+  "extends": "eslint:recommended",
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true
+  },
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "rules": {
+  }
+}
